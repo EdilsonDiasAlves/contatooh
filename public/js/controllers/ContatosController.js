f33cfbb7b5810d9f64fcb3a74af63e658b320311
EdilsonDiasAlves <edilsondiasalves@hotmail.com>
2017-08-09 02:18:30 -0300

    Inclusão de dependência de mais alto nível (angular-resource) para consumir REST endpoints com maior abstração

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,16 +1,23 @@
 angular.module('contatooh').controller('ContatosController', 
-  function($scope, $http){
+  function($scope, $resource){
 
     $scope.contatos = [];
       
     $scope.filtro = '';
     
-    $http.get('/contatos')
-    .success(function(data){
-      $scope.contatos = data;
-    })
-    .error(function(statusText){
-      console.log("Não foi possível obter a lista de contatos");
-      console.log(statusText);
-    });
+    var Contato = $resource('/contatos');
+
+    function buscaContatos(){
+      Contato.query(  
+        function(contatos){
+          $scope.contatos = contatos;
+        },
+        function(erro){
+          console.log("Não foi possível obter a lista de contatos");
+          console.log(erro);
+        }
+      );
+    }
+
+    buscaContatos();
 });
